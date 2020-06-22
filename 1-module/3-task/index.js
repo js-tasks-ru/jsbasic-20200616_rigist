bef7079c9054a72876f2c4544cec1a34ad3ce510
ilgiz <test@test.com>
2020-06-22 21:44:50 +0500

    Решена задача: Сделать первый символ заглавным

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -5,4 +5,20 @@
  */
 function ucFirst(str) {
   // ваш код...
+  if (str == "") {
+    return str;
+  }
+  
+  let strArr = [];
+
+  strArr = str.split("");
+  
+  strArr[0] = strArr[0].toUpperCase();
+  
+  str = strArr.join("");
+  
+  return str;
+
 }
+
+
