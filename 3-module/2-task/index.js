558b679612a8da77e93facc4d6599d782f6a699e
ilgiz <test@test.com>
2020-06-28 20:19:12 +0500

    решена задача: Фильтрация по диапазону

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -6,4 +6,24 @@
  */
 function filterRange(arr, a, b) {
   // ваш код...
+  let NewArr = arr.filter(item => (item >= a && item <= b));
+
+  return NewArr;
+
+  
+
+
+
 }
+
+/* 
+let j = 0;
+let NewArr = [];
+
+for (let i = a; i <= b; i++) {
+  
+  NewArr[j] = arr[i];
+  j++;
+}
+
+return NewArr; */
