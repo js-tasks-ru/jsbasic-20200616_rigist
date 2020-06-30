b7b46cd76c4cde86e98ec4a06916d523ef1720ed
ilgiz <test@test.com>
2020-06-30 18:57:51 +0500

    Решена задача: Функция showSalary(users, age)

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -4,6 +4,38 @@
  * @param {number} age - максимальный возраст
  * @returns {string}
  */
+
 function showSalary(users, age) {
-  // ваш код...
+ 
+  let ArrUserAgeIsTrue = users.filter(item => {if (item.age <= age) return item;})
+
+  let ArrNameAndBalance = ArrUserAgeIsTrue.map(item => {
+    let i = {
+      name: '',
+      balance: 0,
+    };   
+
+    i.name = item.name;
+    i.balance = item.balance;
+
+    return i;
+  } );
+
+
+  let ArrWithoutObj = ArrNameAndBalance.map(item => {
+    return Object.values(item);
+  })
+
+  let NewStr = '';
+
+
+  for (let i = 0; i <  ArrWithoutObj.length; i++) {
+    
+    if (i != (ArrWithoutObj.length )){
+    NewStr += ArrWithoutObj[i].join(", ") + (i != (ArrWithoutObj.length - 1) ? "\n" : "");
+    }
+  
+  }
+  return NewStr;
 }
+
