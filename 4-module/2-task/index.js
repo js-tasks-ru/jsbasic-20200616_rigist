01e544de6e2aa73bc3027606897c2fbbc06f05a2
ilgiz <test@test.com>
2020-07-04 13:07:40 +0500

    Решена задача: Выделите ячейки по диагонали

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -2,5 +2,20 @@
  * @param {HTMLTableElement} table
  * @return {void}
  */
+
+// 12:32  ...12:59... 13:05
+
 function makeDiagonalRed(table) {
+
+
+    for (let i = 0; i < table.rows.length; i++) {
+        for (let j = 0; j < table.rows.length ; j++) {
+            if (i == j){
+                table.rows[i].cells[j].style.backgroundColor = 'red';
+            }
+        }
+    }
+
 }
+
+
