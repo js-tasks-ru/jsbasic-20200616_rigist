e57064fc9d1b31143c7030f5ca6e6a00d0134718
javascriptru <67475495+jsru-1@users.noreply.github.com>
2020-07-25 18:37:12 +0300

    Added solution for task: Учебный проект: Иконка корзины

--- 8-module/1-task/index.js
+++ 8-module/1-task/index.js
@@ -33,12 +33,53 @@ export default class CartIcon {
     }
   }
 
+  updatePosition() {
+
+    if (!this.elem.offsetHeight) {return;} // not visible
+
+    if (!this.initialTopCoord) {
+      this.initialTopCoord = this.elem.getBoundingClientRect().top + window.pageYOffset;
+    }
+
+    if (document.documentElement.clientWidth <= 767) {
+      // mobile: cart is always fixed
+      this.resetPosition();
+      return;
+    }
+
+    let isHeaderCartScrolled = window.pageYOffset > this.initialTopCoord;
+
+    if (isHeaderCartScrolled) {
+      this.fixPosition();
+    } else {
+      this.resetPosition();
+    }
+  }
+
+  fixPosition() {
+    Object.assign(this.elem.style, {
+      position: 'fixed',
+      top: '50px',
+      zIndex: 1e3,
+      left: Math.min(
+        // справа от содержимого (определяем по первому контейнеру в нашей вёрстке)
+        document.querySelector('.container').getBoundingClientRect().right + 20,
+        document.documentElement.clientWidth - this.elem.offsetWidth - 10
+      ) + 'px'
+    });
+  }
+
+  resetPosition() {
+    Object.assign(this.elem.style, {
+      position: '',
+      top: '',
+      left: '',
+      zIndex: ''
+    });
+  }
+
   addEventListeners() {
     document.addEventListener('scroll', () => this.updatePosition());
     window.addEventListener('resize', () => this.updatePosition());
-  }
-
-  updatePosition() {
-    // ваш код ...
   }
 }
