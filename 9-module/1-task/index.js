defd8b825d156dcedcc89d89b118c09898627f41
javascriptru <67475495+jsru-1@users.noreply.github.com>
2020-07-31 15:11:00 +0300

    Added solution for task: Промисификация клика по кнопке

--- 9-module/1-task/index.js
+++ 9-module/1-task/index.js
@@ -5,5 +5,9 @@
  */
 
 export default function promiseClick(button) {
-  // ваш код...
+  return new Promise((resolve) => {
+    button.addEventListener('click', (event) => {
+      resolve(event);
+    }, { once: true });
+  });
 }
